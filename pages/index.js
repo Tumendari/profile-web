17061f597fb94257ab0d0945a2f21bde3f5c9091
Tumendari <82310762+Tumendari@users.noreply.github.com>
2023-01-09 15:14:22 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
-export default function Home() {
-  return (
-    <>
-      <div className=" text-[#19a240]">Hello Tailwind</div>;
-      <div className=" w-1/4 h-screen  bg-cyan-500"></div>
-    </>
-  );
-}
+<html>
+  <head>
+    <title>Profile Web</title>
+    <link rel="stylesheet" type="text/css" href="file.css" />
+  </head>
+  <body>
+    <div id="background">
+      <div class="cart">
+        <div class="header">
+          <img src="wo.jpg" class="profile-img" />
+        </div>
+        <div class="body">
+          <div class="title">Оюунчимэг Түмэндарь</div>
+          <div class="margin">
+            <img src="/icon/email.svg" class="icon" />tdtvmkaum@gmail.com
+          </div>
+          <div class="margin">
+            <img src="/icon/phone.svg" class="icon" />+976 89700455
+          </div>
+          <div class="margin">
+            <img src="/icon/world.svg" class="icon" />tumi.mn
+          </div>
+          <div class="margin">
+            <img src="/icon/location.svg" class="icon" />Ulaanbaatar
+          </div>
+          <div class="footer">
+            <div>
+              <img src="/icon/baske.png" class="icon1" />
+            </div>
+            <div>
+                <img src="/icon/download.png" class="icon1" />
+              </div>
+            <div><img src="/icon/in.png" class="icon1" /></div>
+            <div">
+              <img src="/icon/youtu.png" class="icon1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
